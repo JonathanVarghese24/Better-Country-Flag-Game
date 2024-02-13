0640b06ee5413b79581d86297e261f782e890f56
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-02-13 09:17:25 -0600

    Initial Commit

---
+++ Better Country Flag Game/Better_Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_Country_Flag_GameApp.swift
+//  Better Country Flag Game
+//
+//  Created by Jonathan Varghese on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Better_Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
